a462f39f276706d822be1688c59dde9f3435f5eb
P Sh <moshushi@gmail.com>
2024-02-21 01:32:49 +0300

    Create class and some test

--- src/Figure.py
+++ src/Figure.py
@@ -1,16 +1,46 @@
 class Figure:
-    def __init__(self, name, *args):
-        self.name = name
-        self.area = None
+    def __init__(self,  *args):
+        if len(args) < 1:
+            raise ValueError ("Количество аргументов меньше 1")
+        self.name = "Figure"
+        # self.area = None
         self.args = args
+
+
+    # @property
+    # def number_of_side(self, args):
+    #     if len(args) < 1:
+    #         raise ValueError("Количество аргументов меньше 1")
+    #     return self._number_of_side
 
     @property
     def perimeter(self):
         sum = 0
-        return sum(*self.args)
+        for i in self.args:
+          sum += i
+        return sum
 
-# a = Figure()
-a = Figure(name='Triangle')
-# a.name = 'Triangle'
-print(a.name)
-print(a.perimeter)
+    @property
+    def area(self):
+        return None
+
+    def add_area(figa):
+        if not issubclass(figa, Figure):
+            return ValueError("Добавляемый объект не фигура")
+        return figa
+
+# # a = Figure()
+# a = Figure(name='Triangle')
+# b = Figure('Triangle', 1, 2, 3)
+# # a.name = 'Triangle'
+# print(a.name)
+# print(a.perimeter)
+# print(b.name)
+# print(b.perimeter)
+d = Figure(1)
+print(d.name)
+print(d.perimeter)
+print(d.area)
+d.area = 10
+print(d.area)
+# e = Figure()
---
+++ src/Triangle.py
@@ -0,0 +1,8 @@
+from Figure import Figure
+
+class Triangle(Figure):
+    def __init__(self):
+        super.__init__()
+
+    # @property
+    # def
